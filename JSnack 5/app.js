6b11d872f1371b417dfc78c9084c63c1b8532043
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-07-05 16:52:49 +0200

    aggiunta logica

---
+++ JSnack 5/app.js
@@ -0,0 +1,14 @@
+//Crea un array vuoto. chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array
+
+var array = [];
+var num;
+
+for (var i = 0; i < 5; i++){
+  num = parseInt(prompt("inserisci un numero"));
+
+  if (num % 2 !== 0){
+    array.push(num);
+  }
+}
+
+console.log(array);
